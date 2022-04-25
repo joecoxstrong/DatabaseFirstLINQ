6d9bbbff70b49e2437db9d13e8d57cbdd057505c
joecoxstrong <joecoxstrong@gmail.com>
2022-04-25 10:44:25 -0400

    Rex: wrote function that would filter through a shopping cart and return a total for a single customer

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -22,8 +22,8 @@ namespace DatabaseFirstLINQ
             //ProblemFive();
             //ProblemSix();
             //ProblemSeven();
-            ProblemEight();
-            //ProblemNine();
+            //ProblemEight();
+            ProblemNine();
             //ProblemTen();
             //ProblemEleven();
             //ProblemTwelve();
@@ -141,7 +141,8 @@ Console.WriteLine(product.Name);
             // Write a LINQ query that retreives all of the products in the shopping cart of the user who has the email "oda@gmail.com" and returns the sum of all of the products prices.
             // HINT: End of query will be: .Select(sc => sc.Product.Price).Sum();
             // Then print the total of the shopping cart to the console.
-
+            var cartSum = _context.ShoppingCarts.Include(cs => cs.Product).Where(cs => cs.User.Email == "oda@gmail.com").Select(cs => cs.Product.Price).Sum();
+            Console.WriteLine($"The total for the shopping cart is ${cartSum}");
         }
 
         private void ProblemTen()
